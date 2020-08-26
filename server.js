2fd7551597c8d93c79c05411590ac036abcac85a
lotg515 <dereksmv@gmail.com>
2020-08-26 08:53:48 -0500

    reworked server.js

--- server.js
+++ server.js
@@ -38,15 +38,9 @@ require("./config/passport")(passport);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-
-app.get("/favicon.ico", (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', "favicon.ico"))
-})
+app.get('/', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/build/index.html'));
+});
 
 app.use("/api/users", users);
 
